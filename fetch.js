fe26cdd0ef088a071a33bd0535846434b85accae
hackeronline7 <onlinetohackyou@gmail.com>
2017-11-07 04:03:16 +0530

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -2,7 +2,7 @@ var content = "try again";
 
 function getPrac(name){
 	var client = new XMLHttpRequest();
-	client.open('GET', 'https://skt7.github.io/wp/' + name + '.html');
+	client.open('GET', 'https://hackeronline7.github.io/wp/' + name + '.html');
 	client.onreadystatechange = function() {
 		 content = client.responseText;
 	}
